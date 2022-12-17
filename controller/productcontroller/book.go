718c34ccf877c8bf9e24f73c10f4f834a165eec0
ridhompra <ridhomp29@gmail.com>
2022-12-18 01:10:39 +0700

    feature:CRUD book,employee,visitor

---
+++ controller/productcontroller/book.go
@@ -0,0 +1,113 @@
+package productcontroller
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"project/library_Management/helper"
+	"project/library_Management/models"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+var ResponseJson = helper.ResponseJson
+var ResponseError = helper.ResponseError
+
+func GetAllBook(w http.ResponseWriter, r *http.Request) {
+	var book []models.Book
+
+	if err := models.DB.Find(&book).Error; err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ResponseJson(w, http.StatusOK, book)
+}
+
+func GetBookbyid(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	var book models.Book
+	if err := models.DB.First(&book, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			ResponseError(w, http.StatusNotFound, "Data tidak ditemukan")
+			return
+		default:
+			ResponseError(w, http.StatusNotFound, err.Error())
+			return
+		}
+	}
+	ResponseJson(w, http.StatusOK, book)
+	// log.Println(product)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+	decoder := json.NewDecoder(r.Body)
+	//men decode ke variable book
+	if err := decoder.Decode(&book); err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	//diclose dulu  bodynya
+	defer r.Body.Close()
+	if err := models.DB.Create(&book).Error; err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ResponseJson(w, http.StatusOK, book)
+	log.Printf("Creating success id :%d", book.Id)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	var book models.Book
+	decoder := json.NewDecoder(r.Body)
+	//men decode ke variable book
+	if err := decoder.Decode(&book); err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	//diclose dulu  bodynya
+	defer r.Body.Close()
+	if models.DB.Where("id = ?", id).Updates(&book).RowsAffected == 0 {
+		message := fmt.Sprintf("updating failed, id :%d not found", book.Id)
+		ResponseError(w, http.StatusBadRequest, message)
+		return
+	}
+	book.Id = id
+	ResponseJson(w, http.StatusOK, book)
+	log.Printf("Updating success id:%d", book.Id)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	input := map[string]string{"id": ""}
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&input); err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	//diclose dulu  bodynya
+	defer r.Body.Close()
+	var book models.Book
+	if models.DB.Delete(&book, input["id"]).RowsAffected == 0 {
+		message := fmt.Sprintf("Deleting Failed, id :%d not found", book.Id)
+		ResponseError(w, http.StatusBadRequest, message)
+		return
+	}
+	message := map[string]string{"message": "book berhasil dihapus"}
+	ResponseJson(w, http.StatusOK, message)
+	log.Printf("Deleting success id:%d", book.Id)
+}
